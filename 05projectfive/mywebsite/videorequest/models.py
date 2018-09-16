afba1f0b4d626c791fe1af9823044a56c7ab608c
Priyanka Saggu <priyankasggu11929@gmail.com>
2018-09-16 16:05:41 +0530

    updated 05projectfive

--- 05projectfive/mywebsite/videorequest/models.py
+++ 05projectfive/mywebsite/videorequest/models.py
@@ -4,10 +4,10 @@ from django.utils import timezone
 # Create your models here.
 
 class Video(models.Model):
-    videoTitle = models.CharField(max_length=20)
+    videotitle = models.CharField(max_length=30)
     videodesc = models.TextField()
-    date_added =  models.DateTimeField(default = timezone.now)
+    date_added = models.DateTimeField(default=timezone.now)
 
 
     def __str__(self):
-        return 'Name:{}, ID: {}'.format(self.videotitle, self.id)
+        return 'Name: {}, ID: {}'.format(self.videotitle, self.id)
--- 05projectfive/mywebsite/videorequest/urls.py
+++ 05projectfive/mywebsite/videorequest/urls.py
@@ -1,8 +1,10 @@
+
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
+
     path('', views.index, name='index'),
-    path('vrform/', views.vrform, name='vrform'),
+    path('vrform/', views.vrform, name='vrform')
 ]
