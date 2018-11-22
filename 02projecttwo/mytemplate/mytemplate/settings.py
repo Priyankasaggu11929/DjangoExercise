0b4d85566be98cba79596a067892814df059207f
Priyanka Saggu <priyankasaggu11929@gmail.com>
2018-11-23 04:46:39 +0530

    Update settings.py

--- 02projecttwo/mytemplate/mytemplate/settings.py
+++ 02projecttwo/mytemplate/mytemplate/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8u!(o3maiz7z&lt9!@kg(jnu=w)0run^$%=6ih=p$#g(+#0na2'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
